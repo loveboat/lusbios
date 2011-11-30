4ab44033eb2264b8f134ca2c6042d5cd72fd3b13
lusby <lusbyd@gmail.com>
2011-11-30 16:50:17 +0000

    Added empty class type

---
+++ Calculator/CalculatorBrain.h
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.h
+//  Calculator
+//
+//  Created by David Lusby on 30/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CalculatorBrain : NSObject
+
+@end
---
+++ Calculator/CalculatorBrain.m
@@ -0,0 +1,13 @@
+//
+//  CalculatorBrain.m
+//  Calculator
+//
+//  Created by David Lusby on 30/11/2011.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "CalculatorBrain.h"
+
+@implementation CalculatorBrain
+
+@end
